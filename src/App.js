4ace81f99ac5443a11d101998a38f38f64346b14
Natalie Laing <natalielaing7@gmail.com>
2022-07-06 16:22:52 -0400

    put current components in components folder within src folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import Header from "./Header";
-import TourSearch from "./TourSearch";
-import DisplayData from "./DisplayData";
-import Footer from "./Footer";
+import Header from "./components/Header";
+import TourSearch from "./components/TourSearch";
+import DisplayData from "./components/DisplayData";
+import Footer from "./components/Footer";
 import './App.css';
 
 function App() {
