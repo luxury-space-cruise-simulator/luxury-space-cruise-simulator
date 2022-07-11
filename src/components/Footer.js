95765fb5d9d6b953a8338f751a794edca3ce51b3
Dianna Plaza <dianna.s.plaza@gmail.com>
2022-07-11 00:19:44 -0400

    added footer paragraph and linked everyones portfolio

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,8 @@
 const Footer = () => {
     return (
-        <></>
+        <footer>
+            <p>Created by <a href="https://carolinemanalo.com/">Carolina Manalo</a>, <a href="https://www.diannaplaza.com/">Dianna Plaza</a>, <a href="https://www.moriahvice.com/">Moriah Vice</a>, and <a href="https://www.natalielaing.com/">Natalie Liang</a> at <a href="https://junocollege.com/">Juno College</a> </p>
+        </footer>
     )
 }
 export default Footer;
