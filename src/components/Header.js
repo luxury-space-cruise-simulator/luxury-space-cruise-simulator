63affe604b8b5d703bcc47d20d1ee8c7e5b7a7ff
Vice23 <mvice045@gmail.com>
2022-07-11 09:11:13 -0400

    slight modification to paragraph text

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
             <h2>Mars Edition</h2>
 
             <p>
-                Visit Earth's neighbouring red planet and discover ancient organisms, understand the evolution of the planet, and experience marsquakes in real time! Don't miss out on our luxury ships and on-site destinations, equipped with a full day spa, fine dining, and more!
+                Visit Earth's neighbouring red planet and discover ancient organisms, understand the evolution of the planet, and experience marsquakes in real time! From hosting your dream wedding to a solo excursion, we have packages to suit your needs. Don't miss out on our luxury ships and on-site destinations, equipped with a full day spa, fine dining, and more.
             </p>
             <a href="#search">Book A Trip</a>
 
