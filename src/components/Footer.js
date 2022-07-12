d8d615032fc307aef37ef31d6cc5e2eec0b3e065
Dianna Plaza <dianna.s.plaza@gmail.com>
2022-07-12 17:53:11 -0400

    pulling updated styling branch

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,7 @@
 const Footer = () => {
     return (
         <footer>
-<<<<<<< HEAD
             <p>Created by <a href="https://carolinemanalo.com/">Caroline Manalo</a>, <a href="https://www.diannaplaza.com/">Dianna Plaza</a>, <a href="https://www.moriahvice.com/">Moriah Vice</a>, and <a href="https://www.natalielaing.com/">Natalie Liang</a> at <a href="https://junocollege.com/">Juno College</a> </p>
-=======
-            <p>Created by <a href="https://carolinemanalo.com/">Caroline Manalo</a>, <a href="https://www.diannaplaza.com/">Dianna Plaza</a>, <a href="https://www.moriahvice.com/">Moriah Vice</a>, and <a href="https://www.natalielaing.com/">Natalie Laing</a> at <a href="https://junocollege.com/">Juno College</a> </p>
->>>>>>> ce20b414e855e4705dd099419cba7d99a336fcdb
         </footer>
     )
 }
--- src/components/TourSearch.js
+++ src/components/TourSearch.js
@@ -124,44 +124,37 @@ const TourSearch = (props) => {
     return (
         <section id="search" className="tourSearch">
             <a href="https://www.freepnglogos.com/pics/mars" title="Image from freepnglogos.com"><img src="https://www.freepnglogos.com/uploads/mars-png/mars-transparent-png-stickpng-0.png" width="200" alt="mars image red planet from space" /></a>
+            
             <div className="searchFlex">
-            <h3>Where would you like to explore?</h3>
-            <form  className ="formFlex" onSubmit={(event, chosenTour) => {
-                handleSubmit(event, props.tourDropdown)
-            }}>
-                <select onChange={handleChange} name="tour" id="chosenTour" value={props.tourDropdown}>
-                    <option value="" default disabled>Please choose a tour location</option>
-                    <option value="curiosity">Gale Crater</option>
-                    <option value="spirit">Gusev Crater</option>
-                    <option value="perseverance">Jezero Crater</option>
-                    <option value="opportunity">Meridian Planum</option>
-                </select>
-                {
-                    (count >= 1)
-                        ?
-                        <button onClick={decreaseCount} className="button">Take Me on a Virtual Tour</button>
-                        :
-                        <button onClick={decreaseCount} disabled = {true}className="button">No more tours for you! 😭</button>
+                <h3>Where would you like to explore?</h3>
+                <form  className ="formFlex" onSubmit={(event, chosenTour) => {
+                    handleSubmit(event, props.tourDropdown)
+                    }}>
+                    <select onChange={handleChange} name="tour" id="chosenTour" value={props.tourDropdown}>
+                        <option value="" default disabled>Please choose a tour location</option>
+                        <option value="curiosity">Gale Crater</option>
+                        <option value="spirit">Gusev Crater</option>
+                        <option value="perseverance">Jezero Crater</option>
+                        <option value="opportunity">Meridian Planum</option>
+                    </select>
+                    {
+                        (count >= 1)
+                            ?
+                            <button onClick={decreaseCount} className="button">Take Me on a Virtual Tour</button>
+                            :
+                            <button onClick={decreaseCount} disabled = {true}className="button">No more tours for you! 😭</button>
 
-                }
-<<<<<<< HEAD
+                    }
+                </form>
+            </div>
 
                 <div className="wrapper">
                     <div className="countP">
                         <p>
                             {`You have ${count} virtual tours left
-    for today!`}
+                        for today!`}
                         </p>
                     </div>
-=======
-                </form>
-                </div>
-                <div className="countP">
-                    <p>
-                        {`You have ${count} virtual tours left
-for today!`}
-                    </p>
->>>>>>> ce20b414e855e4705dd099419cba7d99a336fcdb
                 </div>
 
         </section>
