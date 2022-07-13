edf1a3ea8dcdb72bf9140a9be2cd13e64373ed54
CarolineManalo <civymanalo@gmail.com>
2022-07-13 15:56:16 -0400

    added caption to carousel, disclaimer safety text, removed black background

--- src/components/CarouselComponent.js
+++ src/components/CarouselComponent.js
@@ -4,7 +4,6 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 const CarouselComponent = (props) => {
     return (
         <div className="wrapper carouselSize">
-            <p>Virtual Gallery:</p>
             <Carousel centerMode={false} infiniteLoop={true} autoPlay={true} showStatus={false} showIndicators={false} showThumbs={false}>
                     {
                         props.tourLocation.slice(0, 15).map((photo) => {
@@ -12,6 +11,8 @@ const CarouselComponent = (props) => {
                                 <div className="tourImages" key={photo.id}>  
                                     <div className="imgContainer">
                                         <img src={photo.img_src} alt={`Photo taken by ${photo.camera.full_name} on Mars by ${photo.rover.name}`} />
+                                        <p className="legend">{`Photo taken by ${photo.camera.full_name} on Mars by ${photo.rover.name}`}
+                                        </p>
                                     </div>
                                 </div>
                             );
